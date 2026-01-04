2ad90d92633a67538b5236d35e6790dcb1a603f1
Mateusz Smuga <mateusz@smuga.pl>
2026-01-04 14:48:27 +0100

    delete

--- init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.opt.relativenumber = true
-
-require("core.decorations")
-require("core.keymaps")
-require("plugins")
--- lua/plugins/repeat.lua
+++
@@ -1,8 +0,0 @@
--- lua/plugins/qol.lua (or inside your plugins/init.lua)
-return {
-    {
-        "tpope/vim-repeat",
-        event = "VeryLazy",  -- lazy load after startup
-    },
-}
-
