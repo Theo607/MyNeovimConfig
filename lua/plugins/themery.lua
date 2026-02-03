de659e4448ce5ae6021fab43edd3fc2cc13f0f1d
Theo607 <mateusz@smuga.pl>
2026-02-03 12:06:54 +0100

    add: better themes

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -1,74 +1,13 @@
 return {
-  {
-    "zaldih/themery.nvim",
-    lazy = false,
+    "erl-koenig/theme-hub.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        -- Optional: for themes that use lush (will be notified if a theme requires it)
+        "rktjmp/lush.nvim"
+    },
     config = function()
-      local themery = require("themery")
-
-      themery.setup({
-        themes = {
-          -- gruvbox
-          "gruvbox",
-          -- onedark
-          "onedark",
-          -- tokyonight
-          "tokyonight",
-          "tokyonight-day",
-          "tokyonight-storm",
-          "tokyonight-night",
-          -- catppuccin
-          "catppuccin",        -- mocha default
-          "catppuccin-latte",
-          "catppuccin-frappe",
-          "catppuccin-macchiato",
-          -- nord
-          "nord",
-          -- everforest
-          "everforest",
-          -- dracula
-          "dracula",
-          -- vscode
-          "vscode",
-          -- material
-          "material",
-          -- habamax
-          "habamax",
-          -- monokai
-          "monokai",
-          -- nightfox
-          "nightfox",
-          "dayfox",
-          -- rose-pine
-          "rose-pine",
-          "rose-pine-dawn",
-          "rose-pine-moon",
-          -- oxocarbon
-          "oxocarbon",
-          -- kanagawa
-          "kanagawa",
-          "kanagawa-lotus",
-          "kanagawa-wave",
-        },
-        default_theme = "tokyonight",
-      })
-    end
-  },
-
-  -- Popular themes with repositories
-  { "morhetz/gruvbox" },
-  { "navarasu/onedark.nvim" },
-  { "folke/tokyonight.nvim" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "shaunsingh/nord.nvim" },
-  { "sainnhe/everforest" },
-  { "dracula/vim", name = "dracula" },
-  { "Mofiqul/vscode.nvim" },
-  { "marko-cerovac/material.nvim" },
-  { "altercation/vim-colors-solarized", name = "habamax" },
-  { "sickill/vim-monokai" },
-  { "EdenEast/nightfox.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "nyoom-engineering/oxocarbon.nvim" },
-  { "rebelot/kanagawa.nvim" },
+        require("theme-hub").setup({
+            persistent = true,
+        })
+    end,
 }
-
